245a413f79680ec1d53daa1be71968f89c0b5ff4
Jiri Jelinek <146722585+jiri-jelinek@users.noreply.github.com>
2024-01-28 16:44:01 +0100

    deklarace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+* Nazev: ComplexNumberConverter
+* Autor: 
+* Datum: xx.xx.20xx.
+* Popis:
+  Program převede komplexní číslo z agebraického tvaru z = (x + jy) na goniometrický tvar.
+*/
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x, y, abs, radians, degrees;
+
+
+
+    }
+}
