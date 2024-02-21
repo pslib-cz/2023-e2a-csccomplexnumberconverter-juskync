52823b638cc335dae7e961239ce5a7dc5d009a4a
juskync <153064773+juskync@users.noreply.github.com>
2024-02-21 09:32:26 +0100

    Update Program.cs final

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿
+
+
 /*
 * Nazev: ComplexNumberConverter
-* Autor: nn
-* Datum: xx.xx.20xx.
+* Autor: Matej Juska
+* Datum: 21.02.2024
 * Popis:
   Program převede komplexní číslo z agebraického tvaru z = (x + jy) na goniometrický tvar.
 */
-
 
 using System;
 
@@ -14,9 +14,61 @@ class Program
 {
     static void Main()
     {
-        double x, y, abs, radians, degrees; 
+        double x = 0.0, y = 0.0, abs = 0.0, radians = 0.0, degrees = 0.0;
 
 
 
+        Console.WriteLine("Program převede komplexní číslo z algebraického tvaru z = (x + jy) na goniometrický tvar.");
+
+        Console.WriteLine("Pokud zadaš x= 0 a y = 0 program ukončíš");
+        while (true)
+        {
+
+            bool inputOK = false;
+            while (!inputOK)
+            {
+                try
+                {
+                    Console.Write("x=");
+                    x = Double.Parse(Console.ReadLine());
+
+                    Console.Write("y=");
+                    y = Double.Parse(Console.ReadLine());
+                    inputOK = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Syntax Error");
+                }
+
+            }
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("konec konec konec konec");
+                break;
+            }
+
+
+
+
+
+
+
+
+            abs = Math.Sqrt(x * x + y * y);   // absolutní hodnota komplexního čísla
+            radians = Math.Atan2(y, x);       // V C# se používá Math.Atan2 pro výpočet arctan
+            degrees = radians * (180 / Math.PI);  // převod na stupně z radianů
+
+            if (degrees < 0)
+            {
+                degrees += 260;
+            }
+
+            Console.WriteLine($"Goniometrický tvar komplexního čísla je:  z = {abs:F2} ( cos {degrees:F0}° + j sin {degrees:F0}° )");
+        }
     }
 }
+
+
+
